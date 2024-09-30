09dccfad3ce8a717b36f2640dd70b4d105c095d2
Moriya <moriyakatsman@hilma.tech>
2024-09-30 14:53:28 +0300

    log in and sign up functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
-let
+const submitButton = document.querySelector('button');
+
+let enterdUserName = document.getElementById('userName').value;
+let enterdPassword = parseInt(document.getElementById('password').value);
+let users = JSON.parse(localStorage.getItem("users"));
+
+submitButton.addEventListener('click', (e)=>{
+   e.preventDefault();
+    let doseUserNameExist = [false,null];
+    for (let i = 0; i < users.length; i++) {
+        if (enterdUserName === users[i].userName) {
+            doseUserNameExist = [true,i];
+        }
+    }
+    if (doseUserNameExist[0]=== true){
+        if(enterdPassword === parseInt(users[doseUserNameExist[1]].password)){
+            console.log( enterdPassword);
+            alert(`welcome to GameLand ${enterdUserName}`);
+            localStorage.setItem('currentUser',JSON.stringify(users[doseUserNameExist[1]]))
+        }else{
+            alert('password is invaled');
+        }
+    }else{
+        alert("user name dosen't exist");
+    }
+});
+
+submitButton.addEventListener('click',linkToGamePage)
+function linkToGamePage(){
+    location.href="./html/games.html";
+}
--- js/signUp.js
+++ js/signUp.js
@@ -1,16 +1,24 @@
 const submitButton = document.querySelector('button');
-submitButton.addEventListener('click', addNewUser);
+submitButton.addEventListener('click', checkIfValid);
 
-const user = {}
 let userName = document.getElementById('userName')
 let password = document.getElementById('password')
-let verfiyPassword = document.getElementById('verfiyPassword')
 let email = document.getElementById('email')
 let phoneNumber = document.getElementById('phoneNumber')
 let age = document.getElementById('age')
 
+function checkIfValid() {
+    if (userName.value !== '' && password.value !== '') {
+        createNewUser()
+    } else {
+        alert('You must fill user name and password first')
+    }
+}
+
+
+
 // let key = ['userName', 'password', 'email', 'phoneNumber', 'age']
-function addNewUser() {
+function createNewUser() {
     // for (let i = 0; i < key.length; i++) {
     //     let e;
     //     e = document.getElementById(key[i]);
@@ -19,12 +27,30 @@ function addNewUser() {
     //     console.log('e.value: ', e.value);
 
     // }
-    user.userName=userName.value;
-    user.password=password.value;
-    user.verfiyPassword=verfiyPassword.value;
-    user.email=email.value;
-    user.phoneNumber=phoneNumber.value;
-    user.age=age.value;
-    console.log("use ", user);
+    let user = {}
+    user.userName = userName.value;
+    user.password = password.value;
+    user.email = email.value;
+    user.phoneNumber = phoneNumber.value;
+    user.age = age.value;
+    // console.log('user: ', user);
+    addNewUser(user);
 }
-addNewUser();
+
+let users = JSON.parse(localStorage.getItem("users"))
+function addNewUser(user) {
+    if (users === null) {
+        localStorage.setItem("users", JSON.stringify([user]))
+    } else {
+        localStorage.setItem("users", JSON.stringify([...users, user]))
+    }
+    localStorage.setItem('currentUser', JSON.stringify(user))
+    alert(`welcome to GameLand ${user.userName}`);
+    linkToGamePage()
+}
+
+
+function linkToGamePage() {
+    location.href = "../html/games.html";
+}
+console.log(JSON.parse(localStorage.getItem('users')));
