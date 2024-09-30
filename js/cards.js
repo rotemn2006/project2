8c7c22fa31022f8f300b82274b1a0ec9557bffa5
rotemn2006 <rotemneugeboren@hilma.tech>
2024-09-30 14:46:24 +0300

    game changes

--- js/cards.js
+++ js/cards.js
@@ -1,47 +1,47 @@
 let quiz= [
     {
-        ques:"what up",
+        ques:"What is your biggest fear?",
         answer:[
-            {ans:"fine",point:1},
-            {ans:"ok",point:3},
-            {ans:"cool",point:5}
+            {ans:"haircuts",point:1},
+            {ans:"apples",point:3},
+            {ans:"Ocean pollution",point:5}
         ]
     },
 
 
     {
-        ques:"how old are you?",
+        ques:"What is the biggest scam that could happen to you?",
         answer:[
-            {ans:"13",point:1},
-            {ans:"14",point:3},
-            {ans:"15",point:5}
+            {ans:"trip over my own hair",point:1},
+            {ans:"Step on one of my friends",point:3},
+            {ans:"fall out of the boat into the water in a very awkward way",point:5}
         ]
     },
 
     {
-        ques:"how akn",
+        ques:"What kind of pet would you like?",
         answer:[
-            {ans:"fine",point:1},
-            {ans:"rotem",point:3},
-            {ans:"moriya",point:5}
+            {ans:"A male chameleon that likes to hide",point:1},
+            {ans:"A songbird that can sing with me in the same voice",point:3},
+            {ans:"A cute little fish",point:5}
         ]
     },
 
     {
-        ques:4,
+        ques:"What is your biggest dream?",
         answer:[
-            {ans:1,point:1},
-            {ans:2,point:3},
-            {ans:3,point:5}
+            {ans:"To see fireworks on my birthday",point:1},
+            {ans:"manage to overcome the fear of apples",point:3},
+            {ans:"swim with dolphins",point:5}
         ]
     },
 
     {
-        ques:5,
+        ques:"What life lesson did you learn the hard way?",
         answer:[
-            {ans:1,point:1},
-            {ans:2,point:3},
-            {ans:3,point:5}
+            {ans:"Not to trust anyone",point:1},
+            {ans:"Never accept a gift from a stranger",point:3},
+            {ans:"If you find an interesting stone, keep it - it might save the world",point:5}
         ]
     }
 ]
--- js/quiz.js
+++ js/quiz.js
@@ -1,9 +1,4 @@
-// git init
-// git add .
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin git@github.com:rotemn2006/project2.git
-// git push -u origin main
+
 
 let numberOfQuestion=0;
 let quesion= document.getElementById("question");
@@ -14,49 +9,62 @@ let sum=0;
 
 displayQuestion();
 
+//putting onClick on each answer
+answerOne.addEventListener("click",(event)=>updateScore(1));
+answerTwo.addEventListener("click",(event)=>updateScore(3));
+answerThree.addEventListener("click",(event)=>updateScore(5));
 
 
-let pickedAnswer= document.getElementsByClassName("answer");
+//Apdate score
+function updateScore(score){
+    if(parseInt(numberOfQuestion)< parseInt(quiz.length)){
+        sum = sum+score;
+        console.log(sum); 
+        displayQuestion();
+    }
+    else{
+       showResults();
+    }
+}
+   
 
-// for(i=0; i<pickedAnswer.length; i++){
-
-answerOne.addEventListener("click",displayQuestion);
-answerTwo.addEventListener("click",displayQuestion );
-answerThree.addEventListener("click",displayQuestion );
-
-// pickedAnswer[i].addEventListener("click",displayQuestion);
-console.log(pickedAnswer);
-
-
-
-
-
-
+//Passing question
 function displayQuestion( ){
-    // if(numberOfQuestion>quiz.length){
-    //     showResults(); 
-    // }
-    // else{
+ 
+    console.log(numberOfQuestion);
     question.textContent= quiz[numberOfQuestion].ques;
     answerOne.textContent= quiz[numberOfQuestion].answer[0].ans;
     answerTwo.textContent= quiz[numberOfQuestion].answer[1].ans;
     answerThree.textContent= quiz[numberOfQuestion].answer[2].ans;
     numberOfQuestion++;
+}
 
+//Show result
+function showResults(){  
+    console.log("hi!");
+    let result= document.getElementById("footer");
+    let princessImg= document.createElement("img");
+    let imgContainer= document.getElementsByTagName("footer");
+    let playAgain= document.getElementById("playAgain");
+
+    imgContainer[0].appendChild(princessImg);
+
+    if(sum>=4 && sum<9){
+        result.textContent="you are Raponzel!";
+        princessImg.src="../imgs/raponzel.jpg";
     }
-
-
-function updateScore(event){
-// if(event?.target===13)
-
+    else if(sum>9 && sum<14){
+        result.textContent="you are Snow White!";
+        princessImg.src="../imgs/snowhite.jpg";
+    }
+    else{
+        result.textContent="you are Ariel!" ;
+        princessImg.src="../imgs/ariel.jpeg";
+    }
+    playAgain.textContent="play again";
+    console.log(sum);
+   
 }
-    
 
-
-
-function showResults(){
-// let result=document.getElementsByTagName(footer);
-// result[0].textContent="yay!" ;
-}
 
 
